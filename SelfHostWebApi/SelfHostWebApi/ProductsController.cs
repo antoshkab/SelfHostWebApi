a8d3921577265835f25bf9e70bb421a2a4e1c9f9
Anton <antoshkab@hotmail.com>
2015-08-27 10:55:15 +0300

    Implement self host web api controller

---
+++ SelfHostWebApi/SelfHostWebApi/ProductsController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Domain;
+
+namespace Host
+{
+    public class ProductsController : ApiController
+    {
+        private static List<Product> _products = new List<Product>
+        {
+            new Product{ Id = 1, Category = "Категория1", Name = "Продукт1Кат1", Price = 125.00m},
+            new Product{ Id = 2, Category = "Категория1", Name = "Продукт2Кат1", Price = 124.40m},
+            new Product{ Id = 3, Category = "Категория2", Name = "Продукт3Кат2", Price = 1254.02m},
+            new Product{ Id = 4, Category = "Категория3", Name = "Продукт4Кат3", Price = 4345.12m},
+        };
+
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _products;
+        }
+
+
+        public Product GetProductById(int id)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            return product;
+        }
+
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return _products.Where(p => p.Category.Equals(category, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public HttpResponseMessage PostProduct(Product product)
+        {
+            product.Id = _products.Count + 1;
+            _products.Add(product);
+            var response = Request.CreateResponse(HttpStatusCode.Created, product);
+            var uri = Url.Link("API Default", new {id = product.Id});
+            response.Headers.Location = new Uri(uri);
+            return response;
+        }
+
+        public void PutProduct(int id, Product product)
+        {
+            var index = _products.FindIndex(p => p.Id == id);
+            if (index == -1)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            _products.RemoveAt(index);
+            _products.Add(product);
+        }
+
+        public void DeleteProduct(int id)
+        {
+            var index = _products.FindIndex(p => p.Id == id);
+            if (index == -1)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            _products.RemoveAt(index);
+        }
+    }
+}
---
+++ SelfHostWebApi/SelfHostWebApi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration("http://localhost:15300");
+            config.Routes.MapHttpRoute("API Default", "api/{controller}/{id}", new {id = RouteParameter.Optional});
+            using (var server = new HttpSelfHostServer(config))
+            {
+                server.OpenAsync().Wait();
+                Console.WriteLine("Press Enter to stop service and quit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
